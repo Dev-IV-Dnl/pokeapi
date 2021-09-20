2fd015b25119dd8395147025e72b386de13cfe38
Dev-IV-Dnl <mattgiordanengo@gmail.com>
2021-09-20 16:41:27 +0200

    maj

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 let liste = $("#liste")
 
-$(".menu button").click((e) =>{
+$(".menu button").click((e) => {
     console.log("click");
     liste.html("");
     $.get("https://pokeapi.co/api/v2/" + e.target.id, callBack)
 });
 function callBack(settings) {
     console.log(settings);
-    for(let i = 0; i < settings.results.length; i++) {
+    for (let i = 0; i < settings.results.length; i++) {
         console.log(settings.results[i].name);
-        $("<a href='"+settings.results[i].url+"'>"+settings.results[i].name+"</a><br>").appendTo(liste);
+        $("<a href='" + settings.results[i].url + "'>" + settings.results[i].name + "</a><br>").appendTo(liste);
     }
 }
