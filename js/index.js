d184c7348b85f029ac9d41b08133d1e6cc7ead11
Dev-IV-Dnl <mattgiordanengo@gmail.com>
2021-09-21 16:09:54 +0200

    maj 21 fin d'aprem

--- js/index.js
+++ js/index.js
@@ -1,43 +1,66 @@
 let liste = $("#liste");
 let details = $(".details");
-function callBack(settings,id) {
+
+//ma première callBack
+function requeteListe(settings, id) {
     // console.log(id);
     // console.log(settings);
     for (let i in settings.results) {
         // console.log(settings.results[i].name);
-        $("<button class='details' id='details"+i+"'>" + settings.results[i].name + "</button><br>").appendTo(liste);
-        if(id === "berry") {
-            $("#details"+i).click( ()=> {
-                // console.log(settings);
-                details.html("");
-                $.get(settings.results[i].url, callBack2);
-                // console.log(settings.results[i]);
-            });
-        } else if(id === "contest-type") {
-            $("#details"+i).click( ()=> {
-                // console.log(settings);
-                details.html("");
-                $.get(settings.results[i].url, callBack3);
-                // console.log(settings.results[i]);
-            });
+        $("<button class='details' id='details" + i + "'>" + settings.results[i].name + "</button><br>").appendTo(liste);
+        switch (id) {
+            case "berry":
+                $("#details" + i).click(() => {
+                    // console.log(settings);
+                    details.html("");
+                    $.get(settings.results[i].url, requeteDetailBerry);
+                    // console.log(settings.results[i]);
+                });
+                break;
+
+            case "contest-type":
+                $("#details" + i).click(() => {
+                    // console.log(settings);
+                    details.html("");
+                    $.get(settings.results[i].url, requeteDetailContestType);
+                    // console.log(settings.results[i]);
+                });
+                break;
+
+            case "encounter-method":
+                $("#details" + i).click(() => {
+                    // console.log(settings);
+                    details.html("");
+                    $.get(settings.results[i].url, requeteDetailEncounters);
+                    console.log(settings.results);
+                });
+                break
         }
     }
 }
 
-function callBack2(settings) {
+//Ma seconde callBack
+function requeteDetailBerry(settings) {
     // console.log(settings);
     // console.log(settings.firmness.name);
     liste.html("");
-    $("<h3>Name :"+settings.name+"</h3><br><p>taste :"+settings.firmness.name+"</p><p>Read more : "+settings.firmness.url+"</p>").appendTo(liste);
+    $("<h3>Name :" + settings.name + "</h3><br><p>taste :" + settings.firmness.name + "</p><p>Read more : " + settings.firmness.url + "</p>").appendTo(liste);
 }
 
-function callBack3(settings) {
+//Ma troisième callBack
+function requeteDetailContestType(settings) {
     console.log(settings);
-    $("<h3>"+settings.name+"</h3><br><p>Statistique principale : "+settings.names[0].name+"</p>").appendTo(liste);
+    liste.html("");
+    $("<h3>" + settings.name + "</h3><br><p>Il s'agit de la statistique : " + settings.names[0].name + "</p>").appendTo(liste);
+}
+
+function requeteDetailEncounters(settings) {
+    console.log(settings);
+    liste.html("");
+    $("<h3>" + settings.name + "</h3><br><p>On les rencontre dans : " + settings.names[1].name + "</p>").appendTo(liste);
 }
 
 $(".menu button").click(e => {
-    
     liste.html("");
-    $.get("https://pokeapi.co/api/v2/" + e.target.id, s=> callBack(s,e.target.id));
+    $.get("https://pokeapi.co/api/v2/" + e.target.id, s => requeteListe(s, e.target.id));
 });
