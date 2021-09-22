bbf2e94b84612ea648439983ad5adfd2fdb18107
Dev-IV-Dnl <mattgiordanengo@gmail.com>
2021-09-22 12:20:33 +0200

    maj 22 sept

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
+const baseUrl = "https://pokeapi.co/api/v2/";
 let liste = $("#liste");
 let details = $(".details");
 
 //Evènement click permettant d'afficher les listes des baies, encounters, evolutions, generations, objets, lieux, machines, attaques et évidemment les pokemons
 $(".menu button").click(e => {
     liste.html("");
-    $.get("https://pokeapi.co/api/v2/" + e.target.id, s => requeteListe(s, e.target.id));
+    $.get(baseUrl + e.target.id, s => requeteListe(s, e.target.id));
 });
 
 
@@ -39,11 +40,27 @@ function requeteListe(settings, id) {
 
 //Ma 2ème callBack
 function requeteDetailBerry(settings) {
-    for (let b in settings) {
-        liste.html("");
-        $("<h3>Name :" + settings.name + "</h3><br><p>Firmness :" + settings.firmness.name + "</p><p>Item : " + settings.item.name + "</p>").appendTo(liste);
-        console.log(settings);
+    // let parseDetails = JSON.stringify(settings);
+    console.log(settings);
+    liste.html("");
+    // $("<h3>Name :" + settings.name + "</h3><br><p>Firmness :" + settings.firmness.name + "</p><p>Item : " + settings.item.name + "</p>").appendTo(liste);
+    $("<div> Name : " + settings.name + "</div>").appendTo(liste);
+    $("<div> firmness : " + settings.firmness.name + "</div>").appendTo(liste);
+    // for(let f in settings.flavors) {
+    //     $("<div> flavor : " + string(settings.flavors[f].flavor.name) + "</div>").appendTo(liste);
+    // }
+    let txtTmp = "<div> flavor : "
+    for (let f = 0; f <= settings.flavors.length - 2; f++) {
+        txtTmp += settings.flavors[f].flavor.name + ", ";
     }
+    txtTmp += settings.flavors[4].flavor.name;
+    txtTmp += "</div>"
+    $(txtTmp).appendTo(liste);
+    $("<div> Growth time : " + settings.growth_time + "</div>").appendTo(liste);
+    $("<div> Item : " + settings.item.name + "</div>").appendTo(liste);
+    $("<div> Max harvest : " + settings.max_harvest + "</div>").appendTo(liste);
+    $("<div> Natural gift Power : " + settings.natural_gift_power + "</div>").appendTo(liste);
+
 }
 
 //Ma 3ème callBack
@@ -58,6 +75,6 @@ function requeteDetailEncounters(settings) {
     $("<h3>" + settings.name + "</h3><br><p>On les rencontre dans : " + settings.names[1].name + "</p>").appendTo(liste);
 }
 
-// function parse(object) {
-//     JSON.parse(object);
-// }
+function string(object) {
+    return JSON.stringify(object);
+}
